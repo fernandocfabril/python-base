b63029a31c5a2e53124739eff53cbfd4fc7472de
fernandocfabril <fernando.fabril@gmail.com>
2023-08-11 00:52:55 +0000

    Adicionado SHEBANG e um cometário

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello, World !!!")
+#!/usr/bin/env python3
+
+# todas as variaveis de ambiente vão para o python rodar o programa
+# Este programa imprime Hello World
+print("Hello, World !!!") # test-ignore
